fe901981c371f0644054473caddddef212df60d9
drronnie <drronnie85@gmail.com>
2019-02-06 17:52:17 +0200

    fix

--- pull.sh
+++ pull.sh
@@ -1,2 +1,10 @@
 git pull
 systemctl restart home-assistant@homeassistant.service
+
+echo "Waiting home-assistant to launch on 8123..."
+
+while ! nc -z localhost 8123; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+echo "Home-assistant launched"
