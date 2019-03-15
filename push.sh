a58fe69db11638891142ee404412552471d6f8cb
drronnie <drronnie85@gmail.com>
2019-03-15 13:38:47 +0200

    fix

--- push.sh
+++ push.sh
@@ -2,6 +2,8 @@
 # cp -r ./3rdparty/homeassistant-custom-components/broadlink_climate_codes/ ./custom_data/
 # cp -r ./3rdparty/homeassistant-custom-components/broadlink_fan_codes/ ./custom_data/
 # cp -r ./3rdparty/homeassistant-custom-components/broadlink_media_codes/ ./custom_data/
+
 git add .
 git commit -m "fix"
 git push
+ ssh root@192.168.1.8 '/config/pull.sh'
