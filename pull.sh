0f68a3711d46f125527a8103cfacfdd1558a1885
drronnie <drronnie85@gmail.com>
2019-03-15 13:45:18 +0200

    fix

--- pull.sh
+++ pull.sh
@@ -2,12 +2,12 @@ date
 git pull
 hassio homeassistant restart
 
-echo "Waiting home-assistant to launch on 8123..."
-date
+# echo "Waiting home-assistant to launch on 8123..."
+# date
 
-while ! nc -z localhost 8123; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
+# while ! nc -z localhost 8123; do   
+#   sleep 1
+# done
 
-echo "Home-assistant launched"
-date
+# echo "Home-assistant launched"
+# date
